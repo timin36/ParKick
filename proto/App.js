cd3b973dde64fe5e9b11b6122740792a147be50f
timin36 <dmsgodi155@gmail.com>
2021-10-08 10:08:58 +0900

    location added

--- proto/App.js
+++ proto/App.js
@@ -4,23 +4,18 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from "./src/screens/Home";
 import Login from "./src/screens/Login";
 import Circle from "./src/components/Circle";
-import { Permission , PERMISSION_TYPE } from './src/AppPermission';
 
 const Stack = createNativeStackNavigator();
-let locationAccess = false;
 
 export default class App extends Component {
 
-  componentDidMount() {
-      Permission.checkPermission(PERMISSION_TYPE.location) //.then((value)=>{alert(value)})
-  }
 
   render() {
     return (
       <NavigationContainer>
           <Stack.Navigator
             initialRouteName = {"Home"}>
-            <Stack.Screen name="Home" component={Home} locationAccess = {locationAccess} />
+            <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="Circle" component={Circle} />
           </Stack.Navigator>
--- proto/src/screens/Home.js
+++ proto/src/screens/Home.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import {
   StyleSheet,
   View,
   Button,
+  Label,
 } from 'react-native';
 
 import MapView from 'react-native-maps';
+import { Permission , PERMISSION_TYPE } from '../AppPermission';
+import Geolocation from '@react-native-community/geolocation';
+
+async function locationAccess () {
+    Permission.checkPermission(PERMISSION_TYPE.location)//.then((value)=>{console.log("checkPermission value: ",value);})
+}
+
 
 const Home = ({navigation}) => {
+  
+  const [location,setLocation] = useState({"latitude" : 0 ,"longitude": 0});
 
+  locationAccess().then(()=>{console.log("permission check");})
+
+  Geolocation.getCurrentPosition( 
+    pos => { console.log(pos.coords); 
+             setLocation(pos.coords);
+            }, //pos.coords에 위치
+    err => { console.log(err); },    //error 발생시 출력
+    { enableHighAccuracy: true, timeout: 1000, maximumAge: 0, }, );
+    //높은 정확도, timeout, 이전 저장값 사용 시 timeout
+
+  console.log(location);
 
   return (
     <View style = {{flex: 1}}>
@@ -20,7 +41,12 @@ const Home = ({navigation}) => {
         latitudeDelta: 0.005,
         longitudeDelta: 0.002,
       }}
-    />
+    >
+    <MapView.Marker
+        coordinate={{latitude: location.latitude, 
+                     longitude: location.longitude }}
+        title = {'A'}/>
+      </MapView>
     <View style = {styles.btn}>
       <Button title=" + "
               onPress={() => navigation.navigate('Login')}
@@ -28,6 +54,7 @@ const Home = ({navigation}) => {
     </View>
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
