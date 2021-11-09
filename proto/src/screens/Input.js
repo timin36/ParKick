1a56c07978f6e952f50aea79c19bd2885bfd89db
timin36 <dmsgodi155@gmail.com>
2021-11-09 19:14:41 +0900

    pic

--- proto/src/screens/Input.js
+++ proto/src/screens/Input.js
@@ -4,10 +4,12 @@ import {
   Text,
   View,
   TextInput,
+  Image,
   TouchableOpacity
 } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import CheckBox from '@react-native-community/checkbox';
+import MapView from 'react-native-maps';
 
 const Input = ({navigation}) => {
 
@@ -25,8 +27,24 @@ const Input = ({navigation}) => {
         <TextInput
         style={styles.name}
         placeholder="주차장 이름"/>
-       <Text style = {styles.name}>{poi.latitude}</Text>
-       <Text style = {styles.name}>{poi.longitude}</Text>   
+        <Text style={{lineHeight: 10}}>{"\n"}</Text>
+       <View style = {styles.mapwra}>
+       <MapView
+        style = {styles.map}
+        initialRegion={{
+        latitude: poi.latitude,
+        longitude: poi.longitude,
+        latitudeDelta: 0.0005,
+        longitudeDelta: 0.0005,
+        }} 
+        >
+        <MapView.Marker
+        coordinate={{latitude: poi.latitude, 
+                     longitude: poi.longitude }}>
+        <Image source= {require('../../assets/imges/poi.jpg')} style={{ width: 15, height: 15 }} />
+        </MapView.Marker>
+        </MapView></View>
+        <Text style={{lineHeight: 5}}>{"\n"}</Text> 
        <View style = {styles.wrapper}>
          <Text style = {styles.name_1}>주차장 추천</Text><CheckBox value={selected_1} onValueChange ={setSelect1}/> 
          <Text style = {styles.name_1}>주차장 금지</Text><CheckBox value={selected_2} onValueChange ={setSelect2}/>
@@ -43,6 +61,18 @@ const styles = StyleSheet.create({
       backgroundColor: '#D0E6FF',
       alignItems: "center",
       paddingTop: 20,
+  },
+  map:{
+    width: '100%',
+    height: 120,
+  },
+  mapwra:{
+    width: '80%',
+    height: 120,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 20,
+    overflow: 'hidden'
   },
   name: {
     width: '80%',
